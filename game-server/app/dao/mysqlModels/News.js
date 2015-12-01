ce651649c3ab31dbaedfefcc8694cafc05cc5a22
kiendt <kiendt@gviet.vn>
2015-12-01 18:39:09 +0700

    topup

---
+++ game-server/app/dao/mysqlModels/News.js
@@ -0,0 +1,52 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('News', {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey : true,
+      autoIncrement : true
+    },
+    cate: {
+      type: DataTypes.INTEGER(4).UNSIGNED,
+      allowNull: true,
+      index: true
+    },
+    title: {
+      type: DataTypes.STRING(511),
+      allowNull: true
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    status: {
+      type: DataTypes.INTEGER(4).UNSIGNED,
+      allowNull: true,
+      defaultValue: 1
+    },
+    isHot: {
+      type: DataTypes.INTEGER(4).UNSIGNED,
+      allowNull: true,
+      defaultValue: 0
+    },
+    isNews: {
+      type: DataTypes.INTEGER(4).UNSIGNED,
+      allowNull: true,
+      defaultValue: 0
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    },
+    tableName : 'News',
+    indexes:[
+      {
+        columns:["cate","status"]
+      }
+    ]
+  });
+};
